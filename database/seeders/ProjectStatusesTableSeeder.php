8572ab761276397b2f8816567952a905e84298cd
mlab817 <mlab817@gmail.com>
2021-01-13 22:48:52 +0800

    updated seeder

--- database/seeders/ProjectStatusesTableSeeder.php
+++ database/seeders/ProjectStatusesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class ProjectStatusesTableSeeder extends Seeder
@@ -19,15 +20,15 @@ class ProjectStatusesTableSeeder extends Seeder
             [
                'id'             => 1,
                'uuid'           => Str::uuid(),
-               'name'           => 'Proposed',
-               'slug'           => Str::slug('Proposed'),
+               'name'           => 'Ongoing',
+               'slug'           => Str::slug('Ongoing'),
                'description'    => '',
             ],
             [
                 'id'            => 2,
                 'uuid'          => Str::uuid(),
-                'name'          => 'Ongoing',
-                'slug'          => Str::slug('Ongoing'),
+                'name'          => 'Proposed',
+                'slug'          => Str::slug('Proposed'),
                 'description'   => '',
             ],
             [
@@ -40,12 +41,46 @@ class ProjectStatusesTableSeeder extends Seeder
             [
                 'id'            => 4,
                 'uuid'          => Str::uuid(),
-                'name'          => 'Dropped',
-                'slug'          => Str::slug('Dropped'),
+                'name'          => 'Conceptual Stage',
+                'slug'          => Str::slug('Conceptual Stage'),
+                'description'   => '',
+            ],
+            [
+                'id'            => 5,
+                'uuid'          => Str::uuid(),
+                'name'          => 'Approved but not yet ongoing',
+                'slug'          => Str::slug('Approved but not yet ongoing'),
+                'description'   => '',
+            ],
+            [
+                'id'            => 6,
+                'uuid'          => Str::uuid(),
+                'name'          => 'Temporarily Stopped',
+                'slug'          => Str::slug('Temporarily Stopped'),
+                'description'   => '',
+            ],
+            [
+                'id'            => 7,
+                'uuid'          => Str::uuid(),
+                'name'          => 'Cancelled/Terminated',
+                'slug'          => Str::slug('Cancelled/Terminated'),
+                'description'   => '',
+            ],
+            [
+                'id'            => 8,
+                'uuid'          => Str::uuid(),
+                'name'          => 'Dropped from pipeline',
+                'slug'          => Str::slug('Dropped from pipeline'),
                 'description'   => '',
             ],
         ];
 
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('project_statuses')->truncate();
+
         DB::table('project_statuses')->insert($seeds);
+
+        Schema::enableForeignKeyConstraints();
     }
 }
