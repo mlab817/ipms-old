ed455e1961c592b6e0b24859d370d76214576bca
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-02-10 09:09:02 +0800

    removed frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .copy('quasar/dist/spa/index.html', 'resources/views/app.blade.php')
+    .copyDirectory('quasar/dist/spa', 'public');
