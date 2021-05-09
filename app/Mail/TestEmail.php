fe7d468d6c0839af57642e787e189e1f5c82d100
Lester Bolotaolo <mlab817@gmail.com>
2021-05-09 21:48:23 +0800

    email test

--- app/Mail/TestEmail.php
+++ app/Mail/TestEmail.php
@@ -29,6 +29,6 @@ class TestEmail extends Mailable
     public function build()
     {
         return $this->from('admin@site.com')
-                    ->text('testing config');
+                    ->view('emails.test');
     }
 }
---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,1 @@
+Sending test email
