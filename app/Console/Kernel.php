5ce3eb946fa5bd0cf31d687321c71153510808e3
Lester Bolotaolo <mlab817@gmail.com>
2021-05-15 13:31:55 +0800

    schedule was run added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('queue:work --once')->everyMinute()->withoutOverlapping();
+        Log::info('Schedule was run');
     }
 
     /**
