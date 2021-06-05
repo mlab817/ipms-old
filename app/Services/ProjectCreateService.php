e38a8c8b47b154aabd63ce32f083e57b96080902
Lester Bolotaolo <mlab817@gmail.com>
2021-06-05 14:59:19 +0800

    initial commit

--- app/Services/ProjectCreateService.php
+++ app/Services/ProjectCreateService.php
@@ -14,49 +14,49 @@ class ProjectCreateService
     {
         $project = Project::create($data);
 
-        $this->project->feasibility_study()->create($data['feasibility_study']);
+        $project->feasibility_study()->create($data['feasibility_study']);
 
-        $this->project->resettlement_action_plan()->create($data['resettlement_action_plan']);
+        $project->resettlement_action_plan()->create($data['resettlement_action_plan']);
 
-        $this->project->right_of_way()->create($data['right_of_way']);
+        $project->right_of_way()->create($data['right_of_way']);
 
-        $this->project->allocation()->create($data['allocation']);
+        $project->allocation()->create($data['allocation']);
 
-        $this->project->nep()->create($data['nep']);
+        $project->nep()->create($data['nep']);
 
-        $this->project->disbursement()->create($data['disbursement']);
+        $project->disbursement()->create($data['disbursement']);
 
-        $this->project->region_investments()->createMany($data['region_investments']);
+        $project->region_investments()->createMany($data['region_investments']);
 
-        $this->project->fs_investments()->createMany($data['fs_investments']);
+        $project->fs_investments()->createMany($data['fs_investments']);
 
-        $this->project->sdgs()->attach($data['sdgs']);
+        $project->sdgs()->attach($data['sdgs']);
 
-        $this->project->bases()->attach($data['bases']);
+        $project->bases()->attach($data['bases']);
 
-        $this->project->ten_point_agendas()->attach($data['ten_point_agenda']);
+        $project->ten_point_agendas()->attach($data['ten_point_agenda']);
 
-        $this->project->regions()->attach($data['regions']);
+        $project->regions()->attach($data['regions']);
 
-        $this->project->funding_sources()->attach($data['funding_sources']);
+        $project->funding_sources()->attach($data['funding_sources']);
 
-        $this->project->pdp_chapters()->attach($data['pdp_chapters']);
+        $project->pdp_chapters()->attach($data['pdp_chapters']);
 
-        $this->project->pdp_indicators()->attach($data['pdp_indicators']);
+        $project->pdp_indicators()->attach($data['pdp_indicators']);
 
-        $this->project->infrastructure_subsectors()->attach($data['infrastructure_subsectors']);
+        $project->infrastructure_subsectors()->attach($data['infrastructure_subsectors']);
 
-        $this->project->prerequisites()->attach($data['prerequisites']);
+        $project->prerequisites()->attach($data['prerequisites']);
 
-        if ($this->project->has_infra) {
-            $this->project->fs_infrastructures()->createMany($data['fs_infrastructures']);
+        if ($project->has_infra) {
+            $project->fs_infrastructures()->createMany($data['fs_infrastructures']);
         }
 
-        $this->project->created_by = $data['created_by'];
+        $project->created_by = $data['created_by'];
 
         $project->save();
 
-        $this->project->audit_logs()->create([
+        $project->audit_logs()->create([
             'description'  => 'imported',
             'user_id'      => $data['created_by'],
 //            'original'     => [],
@@ -64,6 +64,6 @@ class ProjectCreateService
 //            'host'         => request()->ip() ?? null,
         ]);
 
-        return $project->fresh();
+        return $project->refresh();
     }
 }
