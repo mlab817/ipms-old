462892d7979c4349bb41c345dc9320457e28533a
Lester Bolotaolo <mlab817@gmail.com>
2021-06-10 14:37:24 +0800

    added regions to reports

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -28,6 +28,7 @@
                         <a href="{{ route('reports.implementation_modes') }}" class="list-group-item">By Mode of Implementation</a>
                         <a href="{{ route('reports.funding_sources') }}" class="list-group-item">By Funding Source</a>
                         <a href="{{ route('reports.spatial_coverages') }}" class="list-group-item">By Spatial Coverage</a>
+                        <a href="{{ route('reports.regions') }}" class="list-group-item">By Region</a>
                         <a href="{{ route('reports.offices') }}" class="list-group-item">By Office</a>
                         <a href="{{ route('reports.tiers') }}" class="list-group-item">By Budget Tier (Categorization)</a>
                         <a href="{{ route('reports.pdp_chapters') }}" class="list-group-item">By PDP Chapter</a>
