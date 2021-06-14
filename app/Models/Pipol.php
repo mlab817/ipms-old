d922ac49580930bdb17510be2caef3a846ef2a19
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-06-14 11:12:44 +0800

    added auditable to PIPOL

--- app/Models/Pipol.php
+++ app/Models/Pipol.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,6 +12,7 @@ class Pipol extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Auditable;
 
     const SPATIAL_COVERAGE = [
         'NATIONWIDE'        => 'Nationwide',
